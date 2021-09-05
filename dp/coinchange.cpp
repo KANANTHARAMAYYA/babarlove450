612f4f8e05b82e22c82d0fe85a79d5c280dab3ce
KANANTHARAMAYYA <kanantharamayya@gmail.com>
2021-09-05 10:48:37 -0700

    checking changes init

---
+++ dp/coinchange.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+//recur coinchange
+int coinchange(){
+    
+}
+
+//table coinchange
+int tcoinchange(int arr[],int total,int n){
+
+}
+int main()
+{
+    using namespace std;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,m;
+    cin>>n>>m;
+    n = 4 ; m = 3;
+    S[] = {1,2,3};
+    cout<<"table method : "<<tcoinchange(s,n,m)<<endl;
+    return 0;
+}
