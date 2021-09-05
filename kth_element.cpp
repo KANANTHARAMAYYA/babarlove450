9bfd1772246c4f26713ae290d0c92e7f1f0b280e
KANANTHARAMAYYA <kanantharamayya@gmail.com>
2021-09-05 10:30:31 -0700

    finally working , added before and after to visualize the partitions

--- kth_element.cpp
+++ kth_element.cpp
@@ -1,28 +1,42 @@
 //uses quick select algo
 #include<bits/stdc++.h>
 using namespace std;
-void swap(int *a,int *b){
-    int t = *a;
-    *a = *b;
-    *b = t;
+void swap(int arr[],int a,int b){
+
 }
 int partition(int arr[],int l,int r){
     //choose a random element call it pivot
-    int pivot = arr[r];
-    int i = l;
-    for(int j = l; j< r;++j){
-        if(arr[j]<= pivot){
-            swap(&arr[i],&arr[j]);
-                        i++;
-
+    int pivot_idx = l;
+    int a,b;
+    cout<<"before partition"<<endl;
+      for(int i = l ; i <= r; ++i)
+        cout<<arr[i]<<" ";
+        cout<<endl;
+    for(int i = pivot_idx+1; i <=r; ++i){
+        if(arr[i] <= arr[l]){
+            pivot_idx++;
+            a = i;
+            b = pivot_idx;
+                int t = arr[a];
+    arr[a] = arr[b];
+    arr[b] = t;
         }
-    }
-    swap(&arr[i+1],&arr[r]);
-    return (i+1);
+    }   
+    a = l;
+    b = pivot_idx;
+        int t = arr[a];
+    arr[a] = arr[b];
+    arr[b] = t;
+    cout<<"after partition"<<endl;
+       for(int i = l ; i <= r; ++i)
+        cout<<arr[i]<<" ";
+        cout<<endl;
+ 
+    return pivot_idx;
 }
 
 int kth_element(int arr[],int l,int r, int k){
-    if(k> 0 and k < r - l + 1){
+    if(k> 0 and k <= r - l + 1){
         int i = partition(arr,l,r);
         if(i - l == k - 1)
             return arr[i];
@@ -31,16 +45,15 @@ int kth_element(int arr[],int l,int r, int k){
 
 		return kth_element(arr, i + 1, r, k - i + l - 1);
     }
-    else
     return INT_MAX;
 }
 
 int main(){
     int n,k;
     //cin>>n>>k;
-    k = 4;
-    n = 6;
-    int arr[] = {10, 7, 8, 9, 1, 5};    
+    k = 1;
+    n = 7;
+    int arr[] = {10, 7, 8, 9, 1, 5,11};    
     // for(int i =0 ;i < n; ++i)
     //     cin>>arr[i];
     cout<<kth_element(arr,0,n-1,k)<<endl;
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+before partition
