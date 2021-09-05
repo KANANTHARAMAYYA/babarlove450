199f982cf3000e22bc0753b48dee401402d366df
KANANTHARAMAYYA <kanantharamayya@gmail.com>
2021-09-05 14:51:03 -0700

    a little mistake in the inclusion case . Anyways recursion is weird.

--- dp/coinchange.cpp
+++ dp/coinchange.cpp
@@ -3,12 +3,11 @@ using namespace std;
 
 //recur coinchange
 int coinchange(int arr[],int total,int n){
-
     if(n <= 0 or total < 0)
         return 0;
     if(total == 0)
         return 1;
-    return coinchange(arr,total-arr[n],n-1) + coinchange(arr,total,n-1);
+    return coinchange(arr,total-arr[n],n) + coinchange(arr,total,n-1);
 }
 
 //table coinchange
@@ -17,12 +16,12 @@ int tcoinchange(int arr[],int total,int n){
 }   
 int main()
 {
-    using namespace std;
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int n,m;
-    cin>>n>>m;
-    n = 10 ; m = 3;
+    // cin>>n>>m;
+    n = 4; 
+    m = 3;
     int s[] = {1,2,3};
     cout<<"recursive method : "<<coinchange(s,n,m-1)<<endl;
     return 0;
