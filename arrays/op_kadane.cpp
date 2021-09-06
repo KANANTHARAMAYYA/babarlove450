a6056699b8136803d72cb7496765433ef4ece6b4
KANANTHARAMAYYA <kanantharamayya@gmail.com>
2021-09-05 19:00:11 -0700

    errichto seond method

--- arrays/op_kadane.cpp
+++ arrays/op_kadane.cpp
@@ -11,6 +11,22 @@ int op_kadane(int arr[],int n){
     }
     return ans;
 }
+int presummethod(int arr[],int n){
+    int pref[n];
+    pref[0] = arr[0];
+    for(int i = 1; i < n ;++i){
+        pref[i] = (pref[i-1] + arr[i]);
+    }
+    int min_so_far = 0;
+    int ans = INT_MIN;
+    for(int i = 0; i < n; ++i){
+        if(min_so_far!=INT_MIN){
+            ans = max(ans,pref[i]-min_so_far);
+            min_so_far = min(min_so_far,pref[i]);
+        }
+    }
+    return ans;
+}
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -18,5 +34,6 @@ int main()
     int arr[] = {10, 7, -8, -9, 1, 5,11};   
     int n = sizeof(arr) / sizeof(arr[0]);
     cout<<"max subarray sum : "<<op_kadane(arr,n)<<endl;
+    cout<<"max subarray errichto : "<<presummethod(arr,n)<<endl;
     return 0;
 }
