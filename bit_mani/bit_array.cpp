e378e7674d0d501f9ed0e2489806d3d0079baa42
KANANTHARAMAYYA <kanantharamayya@gmail.com>
2021-09-09 00:00:59 -0700

    dfd

---
+++ bit_mani/bit_array.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int bits[32] = {0};
+    int n;
+    cin>>n;
+    int i = 0;
+    while(i < 32){
+        bits[i] += (1 << n);
+
+    }
+
+    return 0;
+}
